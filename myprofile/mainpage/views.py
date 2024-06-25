eae260c81d8a1480c52eb502548ab7dd75b8812c
ChDimPan <124930886+netunicy@users.noreply.github.com>
2024-06-25 14:29:58 +0300

    Update views.py

--- myprofile/mainpage/views.py
+++ myprofile/mainpage/views.py
@@ -3,16 +3,21 @@ from django.shortcuts import render
 from django.template import loader
 
 def homepage(request):
+  viewer=viewer+1
   return render(request, "homepage.html")
 
 def about_me(request):
+  viewer=viewer+1
   return render(request, "about_me.html")
 
 def education(request):
+  viewer=viewer+1
   return render(request, "education.html")
 
 def skills(request):
+  viewer=viewer+1
   return render(request, "skills.html")
 
 def contact(request):
+  viewer=viewer+1
   return render(request, "contact.html")
