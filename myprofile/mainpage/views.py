65243a6c53b989d9a1de488177b4d1ac72a7e3ce
ChDimPan <124930886+netunicy@users.noreply.github.com>
2024-06-25 15:52:34 +0300

    Update views.py

--- myprofile/mainpage/views.py
+++ myprofile/mainpage/views.py
@@ -1,47 +1,42 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
-
+count = 0 
 def homepage(request):
-  visitor= 0
-  total=visitor+1
-  count+=total
+  global count
+  count+=1
   context = {
         'viewer':  count,
     }
   return render(request, "homepage.html", context)
 
 def about_me(request):
-  visitor= 0
-  total=visitor+1
-  count+=total
+  global count
+  count+=1
   context = {
         'viewer':  count,
     }
   return render(request, "about_me.html", context)
 
 def education(request):
-  visitor= 0
-  total=visitor+1
-  count+=total
+  global count
+  count+=1
   context = {
         'viewer':  count,
     }
   return render(request, "education.html", context)
 
 def skills(request):
-  visitor= 0
-  total=visitor+1
-  count+=total
+  global count
+  count+=1
   context = {
         'viewer':  count,
     }
   return render(request, "skills.html", context)
 
 def contact(request):
-  visitor= 0
-  total=visitor+1
-  count+=total
+  global count
+  count+=1
   context = {
         'viewer':  count,
     }
