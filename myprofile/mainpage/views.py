40d8d09cd228e2d5fd3f6ec3c68494ee86897076
ChDimPan <124930886+netunicy@users.noreply.github.com>
2024-06-25 14:36:51 +0300

    Update views.py

--- myprofile/mainpage/views.py
+++ myprofile/mainpage/views.py
@@ -4,20 +4,35 @@ from django.template import loader
 
 def homepage(request):
   viewer=viewer+1
-  return render(request, "homepage.html")
+  context = {
+        'viewer': viewer,
+    }
+  return render(request, "homepage.html", context)
 
 def about_me(request):
   viewer=viewer+1
-  return render(request, "about_me.html")
+  context = {
+        'viewer': viewer,
+    }
+  return render(request, "about_me.html", context)
 
 def education(request):
   viewer=viewer+1
-  return render(request, "education.html")
+  context = {
+        'viewer': viewer,
+    }
+  return render(request, "education.html", context)
 
 def skills(request):
   viewer=viewer+1
-  return render(request, "skills.html")
+  context = {
+        'viewer': viewer,
+    }
+  return render(request, "skills.html", context)
 
 def contact(request):
   viewer=viewer+1
-  return render(request, "contact.html")
+  context = {
+        'viewer': viewer,
+    }
+  return render(request, "contact.html", context)
