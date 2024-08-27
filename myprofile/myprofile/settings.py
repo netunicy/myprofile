3c65bceaa23bbbb14233627c7bb97dfc49b72918
Pitris Charalambos <charalampospitris1983@gmail.com>
2024-08-27 21:49:22 +0300

    ok

--- myprofile/myprofile/settings.py
+++ myprofile/myprofile/settings.py
@@ -100,7 +100,6 @@ TINYMCE_DEFAULT_CONFIG = {
                "fullscreen preview save print | insertfile image media pageembed template link anchor codesample | "
                "a11ycheck ltr rtl | showcomments addcomment code",
     "custom_undo_redo_levels": 10,
-    "fontsize_formats": "8pt 9pt 10pt 11pt 12pt 13pt 14pt 15pt 16pt 17pt 18pt 19pt 20pt 21pt 22pt 23pt 24pt 25pt 26pt 27pt 28pt 29pt 30pt 31pt 32pt 33pt 34pt 35pt 36pt",
 }
 
 
